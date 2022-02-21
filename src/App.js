e053776c28f5dc66ba2f914ba56a7d66a6aa3521
yuna <nana0744@hyconsy.com>
2022-02-21 15:40:19 +0900

    react netflix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
                     <Route path="/login">
                         <Login />
                     </Route>
-                    <Route path="/movie">
+                    <Route path="/">
                         <Content />
                     </Route>
                 </Switch>
